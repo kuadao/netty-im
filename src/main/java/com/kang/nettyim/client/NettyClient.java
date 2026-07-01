7a5e2123106b0fa62cce51b5f1ea08806edf9014
kang <1035372898@qq.com>
2026-07-01 23:45:20 +0800

    客户端服务端双向通信

---
+++ src/main/java/com/kang/nettyim/client/NettyClient.java
@@ -0,0 +1,29 @@
+package com.kang.nettyim.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class NettyClient {
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        NioEventLoopGroup eventExecutors = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.group(eventExecutors)
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                             @Override
+                             protected void initChannel(SocketChannel ch) {
+                                 ch.pipeline().addLast(new FirstClientHandler());
+                             }
+                         }
+                )
+                .connect("127.0.0.1", 8000)
+                .sync();
+
+    }
+}
---
+++ src/main/java/com/kang/nettyim/server/NettyServer.java
@@ -0,0 +1,32 @@
+package com.kang.nettyim.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class NettyServer {
+
+    public static void main(String[] args) throws InterruptedException {
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(boss, worker)
+                .channel(NioServerSocketChannel.class)
+                .handler(new ChannelInitializer<NioServerSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioServerSocketChannel ch) {
+
+                    }
+                })
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) {
+                        ch.pipeline().addLast(new FirstServerHandler());
+                    }
+                }).bind(8000)
+                .sync();
+    }
+}
