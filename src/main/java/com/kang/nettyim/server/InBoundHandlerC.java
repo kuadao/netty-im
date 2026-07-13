88698a6bf8a46aa5e667ce5cf90c4d63a38a756f
kang <1035372898@qq.com>
2026-07-13 23:39:01 +0800

    Pipeline与ChannelHandler

---
+++ src/main/java/com/kang/nettyim/server/InBoundHandlerC.java
@@ -0,0 +1,13 @@
+package com.kang.nettyim.server;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class InBoundHandlerC extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("InBoundHandlerC: " + msg);
+        ctx.channel().writeAndFlush(msg);
+    }
+}
--- src/main/java/com/kang/nettyim/server/NettyServer.java
+++ src/main/java/com/kang/nettyim/server/NettyServer.java
@@ -24,7 +24,13 @@ public class NettyServer {
                 .childHandler(new ChannelInitializer<SocketChannel>() {
                     @Override
                     protected void initChannel(SocketChannel ch) {
-                        ch.pipeline().addLast(new ServerHandler());
+                        ch.pipeline().addLast(new InBoundHandlerA());
+                        ch.pipeline().addLast(new InBoundHandlerB());
+                        ch.pipeline().addLast(new InBoundHandlerC());
+
+                        ch.pipeline().addLast(new OutboundHandlerA());
+                        ch.pipeline().addLast(new OutboundHandlerB());
+                        ch.pipeline().addLast(new OutboundHandlerC());
                     }
                 }).bind(6000)
                 .sync();
